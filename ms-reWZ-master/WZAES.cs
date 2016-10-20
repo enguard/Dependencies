aa2db9deb85d580c59b951146dd8099572512fa4
enguard <blazing.x@hotmail.com>
2016-10-19 23:10:13 -0400

    .

--- ms-reWZ-master/WZAES.cs
+++ ms-reWZ-master/WZAES.cs
@@ -32,144 +32,186 @@ using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 
-namespace reWZ {
-    internal sealed class WZAES {
-        internal const uint OffsetKey = 0x581C3F6D;
+namespace reWZ
+{
+	internal sealed class WZAES
+	{
+		private byte[] _asciiEncKey;
+		private byte[] _asciiKey;
+		private byte[] _unicodeEncKey;
+		private byte[] _unicodeKey;
+		private byte[] _wzKey;
 
-        private static readonly byte[] AESKey = {
-            0x13, 0x00, 0x00, 0x00, 0x08, 0x00, 0x00, 0x00, 0x06, 0x00, 0x00, 0x00,
-            0xB4, 0x00, 0x00, 0x00, 0x1B, 0x00, 0x00, 0x00, 0x0F, 0x00, 0x00, 0x00, 0x33, 0x00, 0x00, 0x00, 0x52, 0x00,
-            0x00, 0x00
-        };
+		private readonly WZVariant _version;
 
-        private static readonly byte[] GMSIV = {
-            0x4D, 0x23, 0xC7, 0x2B, 0x4D, 0x23, 0xC7, 0x2B, 0x4D, 0x23, 0xC7, 0x2B,
-            0x4D, 0x23, 0xC7, 0x2B
-        };
+		internal WZAES(WZVariant version)
+		{
+			_version = version;
+			GenerateKeys(0x10000);
+		}
 
-        private static readonly byte[] KMSIV = {
-            0xB9, 0x7D, 0x63, 0xE9, 0xB9, 0x7D, 0x63, 0xE9, 0xB9, 0x7D, 0x63, 0xE9,
-            0xB9, 0x7D, 0x63, 0xE9
-        };
+		private void GenerateKeys(int length)
+		{
+			_wzKey = GetWZKey(_version, length);
+			byte[] asciiKey = new byte[_wzKey.Length];
+			byte[] unicodeKey = new byte[_wzKey.Length];
+			byte[] asciiEncKey = new byte[_wzKey.Length];
+			byte[] unicodeEncKey = new byte[_wzKey.Length];
+			unchecked
+			{
+				byte mask = 0xAA;
+				for (int i = 0; i < _wzKey.Length; ++i, ++mask)
+				{
+					asciiKey[i] = mask;
+					asciiEncKey[i] = (byte)(_wzKey[i] ^ mask);
+				}
+				ushort umask = 0xAAAA;
+				for (int i = 0; i < _wzKey.Length / 2; i += 2, ++umask)
+				{
+					unicodeKey[i] = (byte)(umask & 0xFF);
+					unicodeKey[i + 1] = (byte)((umask & 0xFF00) >> 8);
+					unicodeEncKey[i] = (byte)(_wzKey[i] ^ unicodeKey[i]);
+					unicodeEncKey[i + 1] = (byte)(_wzKey[i + 1] ^ unicodeKey[i + 1]);
+				}
+			}
+			_asciiKey = asciiKey;
+			_unicodeKey = unicodeKey;
+			_asciiEncKey = asciiEncKey;
+			_unicodeEncKey = unicodeEncKey;
+		}
 
-        private readonly byte[] _asciiEncKey;
-        private readonly byte[] _asciiKey;
-        private readonly byte[] _unicodeEncKey;
-        private readonly byte[] _unicodeKey;
-        private readonly byte[] _wzKey;
+		internal string DecryptASCIIString(byte[] asciiBytes, bool encrypted = true)
+		{
+			CheckKeyLength(asciiBytes.Length);
+			return Encoding.ASCII.GetString(DecryptData(asciiBytes, encrypted ? _asciiEncKey : _asciiKey));
+		}
 
-        internal WZAES(WZVariant version) {
-            _wzKey = GetWZKey(version);
-            _asciiKey = new byte[_wzKey.Length];
-            _unicodeKey = new byte[_wzKey.Length];
-            _asciiEncKey = new byte[_wzKey.Length];
-            _unicodeEncKey = new byte[_wzKey.Length];
-            unchecked {
-                byte mask = 0xAA;
-                for (int i = 0; i < _wzKey.Length; ++i, ++mask) {
-                    _asciiKey[i] = mask;
-                    _asciiEncKey[i] = (byte) (_wzKey[i] ^ mask);
-                }
-                ushort umask = 0xAAAA;
-                for (int i = 0; i < _wzKey.Length/2; i += 2, ++umask) {
-                    _unicodeKey[i] = (byte) (umask & 0xFF);
-                    _unicodeKey[i + 1] = (byte) ((umask & 0xFF00) >> 8);
-                    _unicodeEncKey[i] = (byte) (_wzKey[i] ^ _unicodeKey[i]);
-                    _unicodeEncKey[i + 1] = (byte) (_wzKey[i + 1] ^ _unicodeKey[i + 1]);
-                }
-            }
-        }
+		internal string DecryptUnicodeString(byte[] ushortChars, bool encrypted = true)
+		{
+			CheckKeyLength(ushortChars.Length);
+			return Encoding.Unicode.GetString(DecryptData(ushortChars, encrypted ? _unicodeEncKey : _unicodeKey));
+		}
 
-        private static byte[] GetWZKey(WZVariant version) {
-            switch ((int) version) {
-                case 0:
-                    return GenerateKey(KMSIV, AESKey);
-                case 1:
-                    return GenerateKey(GMSIV, AESKey);
-                case 2:
-                    return new byte[0x10000];
-                default:
-                    throw new ArgumentException("Invalid WZ variant passed.", nameof(version));
-            }
-        }
+		internal byte[] DecryptBytes(byte[] bytes)
+		{
+			CheckKeyLength(bytes.Length);
+			return DecryptData(bytes, _wzKey);
+		}
 
-        private static byte[] GenerateKey(byte[] iv, byte[] aesKey) {
-            using (MemoryStream memStream = new MemoryStream(0x10000)) {
-                using (AesManaged aem = new AesManaged {KeySize = 256, Key = aesKey, Mode = CipherMode.CBC, IV = iv}) {
-                    using (
-                        CryptoStream cStream = new CryptoStream(memStream, aem.CreateEncryptor(), CryptoStreamMode.Write)
-                        ) {
-                        cStream.Write(new byte[0x10000], 0, 0x10000);
-                        cStream.Flush();
-                        return memStream.ToArray();
-                    }
-                }
-            }
-        }
+		private void CheckKeyLength(int length)
+		{
+			if (_wzKey.Length < length)
+			{
+				GenerateKeys(length);
+			}
+		}
 
-        internal string DecryptASCIIString(byte[] asciiBytes, bool encrypted = true) {
-            return Encoding.ASCII.GetString(DecryptData(asciiBytes, encrypted ? _asciiEncKey : _asciiKey));
-        }
+		internal const uint OffsetKey = 0x581C3F6D;
 
-        internal string DecryptUnicodeString(byte[] ushortChars, bool encrypted = true) {
-            return Encoding.Unicode.GetString(DecryptData(ushortChars, encrypted ? _unicodeEncKey : _unicodeKey));
-        }
+		private static readonly byte[] AESKey = {
+			0x13, 0x00, 0x00, 0x00, 0x08, 0x00, 0x00, 0x00, 0x06, 0x00, 0x00, 0x00,
+			0xB4, 0x00, 0x00, 0x00, 0x1B, 0x00, 0x00, 0x00, 0x0F, 0x00, 0x00, 0x00, 0x33, 0x00, 0x00, 0x00, 0x52, 0x00,
+			0x00, 0x00
+		};
 
-        internal byte[] DecryptBytes(byte[] bytes) {
-            return DecryptData(bytes, _wzKey);
-        }
+		private static readonly byte[] GMSIV = {
+			0x4D, 0x23, 0xC7, 0x2B, 0x4D, 0x23, 0xC7, 0x2B, 0x4D, 0x23, 0xC7, 0x2B,
+			0x4D, 0x23, 0xC7, 0x2B
+		};
 
-        private static unsafe byte[] DecryptData(byte[] data, byte[] key) {
-            // TODO: generate more bytes on demand
-            if (data.Length > key.Length) {
-                throw new NotSupportedException(
-                    $"Cannot decrypt data longer than {key.Length} characters. Please report this!");
-            }
+		private static readonly byte[] KMSIV = {
+			0xB9, 0x7D, 0x63, 0xE9, 0xB9, 0x7D, 0x63, 0xE9, 0xB9, 0x7D, 0x63, 0xE9,
+			0xB9, 0x7D, 0x63, 0xE9
+		};
 
-            fixed (byte* c = data, k = key) {
-                byte* d = c, l = k, e = d + data.Length;
-                while (d < e) {
-                    *d++ ^= *l++;
-                }
-            }
+		private static byte[] GetWZKey(WZVariant version, int length)
+		{
+			length = (length & ~15) + ((length & 15) > 0 ? 16 : 0);
+			switch ((int)version)
+			{
+				case 0:
+					return GenerateKey(KMSIV, AESKey, length);
+				case 1:
+					return GenerateKey(GMSIV, AESKey, length);
+				case 2:
+					return new byte[length];
+				default:
+					throw new ArgumentException("Invalid WZ variant passed.", nameof(version));
+			}
+		}
 
-            return data;
-        }
-    }
+		private static byte[] GenerateKey(byte[] iv, byte[] aesKey, int length)
+		{
+			using (MemoryStream memStream = new MemoryStream(length))
+			{
+				using (AesManaged aem = new AesManaged { KeySize = 256, Key = aesKey, Mode = CipherMode.CBC, IV = iv })
+				{
+					using (
+						CryptoStream cStream = new CryptoStream(memStream, aem.CreateEncryptor(), CryptoStreamMode.Write)
+						)
+					{
+						cStream.Write(new byte[length], 0, length);
+						cStream.Flush();
+						return memStream.ToArray();
+					}
+				}
+			}
+		}
 
-    /// <summary>This enum is used to specify the WZ key to be used.</summary>
-    public enum WZVariant {
-        /// <summary>MapleStory SEA</summary>
-        MSEA = 0,
+		private static unsafe byte[] DecryptData(byte[] data, byte[] key)
+		{
+			if (data.Length > key.Length)
+			{
+				throw new InvalidOperationException("data.Length > key.Length; not supposed to happen, please report this to reWZ");
+			}
 
-        /// <summary>Korea MapleStory</summary>
-        KMS = 0,
+			fixed (byte* c = data, k = key)
+			{
+				byte* d = c, l = k, e = d + data.Length;
+				while (d < e)
+				{
+					*d++ ^= *l++;
+				}
+			}
 
-        /// <summary>Korea MapleStory (Tespia)</summary>
-        KMST = 0,
+			return data;
+		}
+	}
 
-        /// <summary>Japan MapleStory</summary>
-        JMS = 0,
+	/// <summary>This enum is used to specify the WZ key to be used.</summary>
+	public enum WZVariant
+	{
+		/// <summary>MapleStory SEA</summary>
+		MSEA = 0,
 
-        /// <summary>Japan MapleStory (Tespia)</summary>
-        JMST = 0,
+		/// <summary>Korea MapleStory</summary>
+		KMS = 0,
 
-        /// <summary>Europe MapleStory</summary>
-        EMS = 0,
+		/// <summary>Korea MapleStory (Tespia)</summary>
+		KMST = 0,
 
-        /// <summary>Global MapleStory</summary>
-        GMS = 1,
+		/// <summary>Japan MapleStory</summary>
+		JMS = 0,
 
-        /// <summary>Global MapleStory (Tespia)</summary>
-        GMST = 1,
+		/// <summary>Japan MapleStory (Tespia)</summary>
+		JMST = 0,
 
-        /// <summary>Taiwan MapleStory</summary>
-        TMS = 1,
+		/// <summary>Europe MapleStory</summary>
+		EMS = 0,
 
-        /// <summary>Brazil MapleStory</summary>
-        BMS = 2,
+		/// <summary>Global MapleStory</summary>
+		GMS = 1,
 
-        /// <summary>Classic MapleStory (Data.wz)</summary>
-        Classic = 2
-    }
+		/// <summary>Global MapleStory (Tespia)</summary>
+		GMST = 1,
+
+		/// <summary>Taiwan MapleStory</summary>
+		TMS = 1,
+
+		/// <summary>Brazil MapleStory</summary>
+		BMS = 2,
+
+		/// <summary>Classic MapleStory (Data.wz)</summary>
+		Classic = 2
+	}
 }
